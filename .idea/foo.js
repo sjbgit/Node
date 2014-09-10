8935be70a07d44169535d1a730fb01f48b9be450
sjbgit <sjb_strat@hotmail.com>
2014-09-10 13:46:35 -0500

    exports

--- .idea/foo.js
+++ .idea/foo.js
@@ -4,4 +4,12 @@
 var circle = require('./circle.js');
 console.log('Area of circle with r = 4 is: ' + circle.area(4));
 
+var http = require('http');
+
+var square = require('./square.js');
+
+var mySquare = square(2);
+console.log('The area of my square is ' + mySquare.area());
+
+
 
---
+++ .idea/square.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sbunke on 9/10/2014.
+ */
+module.exports = function(width) {
+    return {
+        area: function() {
+            return width * width;
+        }
+    }
+}
